a7d101567f182271b3a86e1e63146cbc06ae3938
W A I - PINKYBEAR <16081998+waihihihi@users.noreply.github.com>
2019-04-24 15:34:36 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://localhost/sahabatmasjid/';
+$config['base_url'] = 'http://localhost/TB_Weblan/';
 
 /*
 |--------------------------------------------------------------------------
