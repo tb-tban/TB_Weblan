3e3fa6ad8bdf2e5f1a0baf0bdc046bcff90869b4
W A I - PINKYBEAR <16081998+waihihihi@users.noreply.github.com>
2019-04-24 15:42:47 +0800

    tes

---
+++ application/modules/tes/controllers/Tes.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tes extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('tes_v', FALSE);
+	}
+
+}
+
+/* End of file tes.php */
+/* Location: ./application/modules/tes/controllers/tes.php */
